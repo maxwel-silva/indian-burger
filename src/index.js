078a649d5b4651d90863347656b2a560ba2a2a70
maxwel-silva <maxwel.santos4@etec.sp.gov.br>
2020-05-28 00:03:24 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react'
+import { Text, View, TextInput, TouchableOpacity } from 'react-native'
 
 import Food from './Food'
 
