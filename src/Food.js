3a147b0b42277ae7828e1f074a925348c8beafb4
maxwel-silva <maxwel.santos4@etec.sp.gov.br>
2020-05-28 23:31:39 -0300

    first commit

--- src/Food.js
+++ src/Food.js
@@ -42,12 +42,12 @@ export default class App extends Component {
       <ScrollView>
         <View style={{ flex: 1, backgroundColor: "#f2f2f2" }}>
           <View style={{ width: width, alignItems: 'center' }} >
-            <Image style={{ height: 60, width: width / 2, margin: 10, marginTop: 50 }} resizeMode='center' source={require("../image/indian.png")} />
+            <Image style={{ height: 60, width: width / 2, margin: 10, marginTop: 50 }} resizeMode="contain" source={require("../image/foodapp.png")} />
             <Swiper style={{ height: width / 2 }} showsButtons={false} autoplay={true} autoplayTimeout={2}>
               {
                 this.state.dataBanner.map((itembann) => {
                   return (
-                    <Image style={styles.imageBanner} resizeMode='center' source={{ uri: itembann }} />
+                    <Image style={styles.imageBanner} resizeMode="contain" source={{ uri: itembann }} />
                   )
                 })
               }
@@ -56,7 +56,7 @@ export default class App extends Component {
           </View>
 
           <View style={{ width: width, borderRadius: 20, paddingVertical: 20, backgroundColor: 'white' }}>
-            <Text style={styles.titleCatg}>  Categorias</Text>
+            <Text style={styles.titleCatg}>  Categories</Text>
             <FlatList
               horizontal={true}
               data={this.state.dataCategories}
@@ -86,7 +86,7 @@ export default class App extends Component {
           style={{ width: 100, height: 80 }}
           resizeMode="contain"
           source={{ uri: item.image }} />
-        <Text style={{ fontWeight: 'bold', fontSize: 22 }}>{item.name}</Text>
+        <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{item.name}</Text>
       </TouchableOpacity>
     )
   }
@@ -101,11 +101,11 @@ export default class App extends Component {
             resizeMode="contain"
             source={{ uri: item.image }} />
           <View style={{ height: ((width / 2) - 20) - 90, backgroundColor: 'transparent', width: ((width / 2) - 20) - 10 }} />
-          <Text style={{ fontWeight: 'bold', fontSize: 22, textAlign: 'center' }}>
+          <Text style={{ fontWeight: 'bold', fontSize: 20, textAlign: 'center' }}>
             {item.name}
           </Text>
           <Text>Descp Food and Details</Text>
-          <Text style={{ fontSize: 20, color: "green" }}>${item.price}</Text>
+          <Text style={{ fontSize: 25, color: "green" }}>${item.price}</Text>
         </TouchableOpacity>
       )
     }
@@ -114,42 +114,46 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   imageBanner: {
-    height: width / 2,
-    width: width - 40,
-    marginHorizontal: 20
+    height:width/2,
+    width:width-40,
+    borderRadius:10,
+    marginHorizontal:20,
   },
 
-  divCategorie: {
-    backgroundColor: 'red',
-    margin: 5, alignItems: 'center',
-    padding: 10
+  divCategorie:{
+    backgroundColor:'red',
+    margin:5, 
+    alignItems:'center',
+    borderRadius:10,
+    padding:10
   },
 
-  titleCatg: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 10
+  titleCatg:{
+    fontSize:30,
+    fontWeight:'bold',
+    textAlign:'center',
+    marginBottom:10
   },
 
-  imageFood: {
-    width: ((width / 2) - 20) - 10,
-    height: ((width / 2) - 20) - 30,
-    backgroundColor: 'transparent',
-    position: 'absolute',
-    top: -45
+  imageFood:{
+    width:((width/2)-20)-10,
+    height:((width/2)-20)-30,
+    backgroundColor:'transparent',
+    position:'absolute',
+    top:-45
   },
 
-  divFood: {
-    width: (width / 2) - 20,
-    padding: 10,
-    marginTop: 55,
-    marginBottom: 5,
-    marginLeft: 14,
-    alignItems: 'center',
-    elevation: 8,
-    shadowOpacity: 0.3,
-    shadowRadius: 50,
-    backgroundColor: 'white',
+  divFood:{
+    width:(width/2)-20,
+    padding:10,
+    borderRadius:10,
+    marginTop:55,
+    marginBottom:5,
+    marginLeft: 13.1,
+    alignItems:'center',
+    elevation:8,
+    shadowOpacity:0.3,
+    shadowRadius:50,
+    backgroundColor:'white',
   }
 })
